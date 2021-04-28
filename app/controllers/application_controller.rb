5c253baa507ba34d9f17cbd84f88ad1d0ac804cd
anhdao21 <adao0@chicagobooth.edu>
2021-04-28 18:36:14 +0000

    Still working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play
+    moves = ["rock", "paper", "scissors"]
+    computer_move = moves.sample
+    human_move=params.fetch("hand")
+    
+    if computer_move == human_move
+      outcome = "tied"
+    elsif (computer_move == "paper" && human_move == "rock") || (computer_move == "rock" && human_move == "scissors") || (computer_move == "scissors" && human_move == "paper")
+      outcome = "lost"
+    else
+      outcome = "won"
+    end
+
+    full_message = "You played " + human_move + ". They played " + computer_move + ". You " + outcome + "!"
+
+    render({ :plain =>  full_message })
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-
-
+    get("/:hand", { :controller => "application", :action => "play" })
+    # get("/", { :controller => "application", :action => "homepage" })
 end
